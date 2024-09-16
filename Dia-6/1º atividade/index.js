3d0ebd4713420812903898f4fc071c0c0237859c
Benson Henrique <benson.henriquesilva@gmail.com>
2024-09-16 18:55:48 -0300

    Resolvendo as atividades do dia 6

---
+++ Dia-6/1º atividade/index.js
@@ -0,0 +1,23 @@
+//Exercicio 1
+
+let contagem = Number(prompt("Digite um número:"))
+
+for(let num=0; num <=contagem; num++){
+    console.log(num)
+
+}
+
+//Exercio 2
+
+
+for (let soma = 0; soma <= 50; soma+=5) {
+    console.log(soma)
+    
+}
+    
+
+//Exercio 3
+
+for (let inver = 50; inver >= 0; inver-=5) {
+    console.log(inver)
+}
---
+++ Dia-6/2º atividade/index.js
@@ -0,0 +1,10 @@
+let re = parseInt(prompt("Digite um número:"))
+
+    for(let i = re; i <= re +2; i++){
+        console.log(i)
+
+            for(let a = 0; a <= 10; a++){
+                console.log(i + " x " + a + " = " + (i * a))
+
+            }
+    }
